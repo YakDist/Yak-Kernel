90bf4d03ba319d154dce193e87b0dac1eefc03cc
Vincent Schweiger <vincent.schweiger@icloud.com>
2026-03-15 17:50:37 +0100

    fix: kconfig could not run because build/kconfig was missing.

--- kernel/lib/assert.cpp
+++ kernel/lib/assert.cpp
@@ -1,7 +1,11 @@
 #include <assert.h>
+#include <yak/log.h>
 
 extern "C" [[gnu::noreturn]]
 void __assert_fail(const char *assertion, const char *file, unsigned int line,
                    const char *function) {
-  asm volatile("cli; hlt");
+  pr_error("Assertion failure: %s at %s:%u in %s\n", assertion, file, line,
+           function);
+  for (;;)
+    asm volatile("cli; hlt");
 }
