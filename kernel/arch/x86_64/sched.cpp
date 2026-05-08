bb860147882340c12e11d931ad632b147447b84f
Vincent Schweiger <vincent.schweiger@icloud.com>
2026-05-08 11:49:19 +0200

    fix: assigned ctx2 to r13 instead of r14

--- kernel/arch/x86_64/sched.cpp
+++ kernel/arch/x86_64/sched.cpp
@@ -13,30 +13,30 @@ void __x86_sched_trampoline();
 
 void Thread::init_context(void *kstack_top, ThreadEntryFn entry, void *ctx1,
                           void *ctx2) {
-  kernel_stack_top = kstack_top;
+  kernel_stack_top_ = kstack_top;
 
   uint64_t *sp = reinterpret_cast<uint64_t *>(kstack_top);
 
   sp -= 2;
   *sp = (uint64_t) __x86_sched_trampoline;
 
-  md.rsp = (uint64_t) sp;
-  md.r12 = (uint64_t) entry;
-  md.r13 = (uint64_t) ctx1;
-  md.r13 = (uint64_t) ctx2;
+  md.rsp = reinterpret_cast<uintptr_t>(sp);
+  md.r12 = reinterpret_cast<uintptr_t>(entry);
+  md.r13 = reinterpret_cast<uintptr_t>(ctx1);
+  md.r14 = reinterpret_cast<uintptr_t>(ctx2);
 
-  if (is_user) {
+  if (is_user_) {
     panic("FPU alloc");
   }
 }
 
 namespace arch {
 void sched_switch(Thread *current, Thread *thread) {
-  if (current->is_user) {
+  if (current->is_user_) {
     panic("FPU save");
   }
 
-  if (thread->is_user) {
+  if (thread->is_user_) {
     panic("tss, fpu -> user setup");
   }
 
